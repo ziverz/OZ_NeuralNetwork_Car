f7d10c3f8a3116398b5c00c5823d9938e656ddca
orabbott <orabbott@gmail.com>
2026-04-22 12:31:02 -0500

    Fixed dnn.py

--- DNN/dnn.py
+++ DNN/dnn.py
@@ -15,7 +15,7 @@ import math
 import numpy as np
 import serial
 
-ser1 = serial.Serial("/dev/ttyAMA1", 115200)
+ser = serial.Serial("/dev/ttyAMA1", 115200)
 
 # Radian <-> Degree conversion functions
 def deg2rad(deg):
